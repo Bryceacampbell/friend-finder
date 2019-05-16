3a519bc38f4eb92ee9a4a926acded62cecab0a4e
John and Kari <bryceacampbell7@gmail.com>
2019-05-15 21:31:35 -0700

    added some styling

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,23 +6,40 @@ module.exports = function (app) {
         res.json(friendsData);
     });
 
-    app.post("/api/friends", function (req, res) {
+    app.post('/api/friends', function (req, res) {
 
         let newFriend = req.body;
-        
-        friendsData.push(newFriend);
+        let newFriendScores = newFriend.scores;
 
-        let comparedScoresArray = [];
+        let matchName = '';
+        let matchImage = '';
+        let totalDifference = 10000;
 
         for (let i = 0; i < friendsData.length; i++) {
 
-            let comparedScore = newFriend.totalScore - friendsData[i].totalScore;
-            comparedScoresArray.push(Math.abs(comparedScore));
-        }
-        console.log("compared scores array: " + comparedScoresArray);
+            let difference = 0;
 
-        console.log(Math.min.apply(null, comparedScoresArray));
+            for (let j = 0; j < newFriendScores.length; j++) {
+                difference += Math.abs(friendsData[i].scores[j] - newFriendScores[j]);
+            }
 
+            if (difference < totalDifference) {
+
+                totalDifference = difference;
+                matchName = friendsData[i].name;
+                matchImage = friendsData[i].photo;
+            }
+        };
+
+        friendsData.push(newFriend);
+
+        res.json({ matchName: matchName, matchImage: matchImage });
     });
 };
+
+
+
+
+
+
 
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+let path = require("path");
+app.use(express.static(path.join(__dirname, "./app/public")));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
